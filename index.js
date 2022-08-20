f1ee7e340fc966b0e31c231ae8a9b481533b30a1
Ronald kubasu <ronald.kubasu@student.moringaschool.com>
2022-08-20 12:38:40 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, s,end){
+    let copy = {...employee}
+    copy[s] = end
+    return copy
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, s, end){
+    employee[s] = end
+    return(employee)
+}
+
+function deleteFromEmployeeByKey(employee, s){
+    let copy = {...employee}
+    delete(copy[s])
+    return copy
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, s){
+    delete(employee[s])
+    return employee
+}
